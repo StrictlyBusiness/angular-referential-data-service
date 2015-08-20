c4fbf8849e07862f0a3537d761047c470649c8e4
Bill Rawlinson <bill.rawlinson@sbcs.com>
2015-08-20 15:55:18 -0400

    add proxy path so test works

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,13 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    // we need this to override the way base is in config.js in order for our
+    // tests to load properly within Karma runner which uses base instead of /
+    // as the baseURL
+    proxies: {
+      '/' : '/base'
+    },
+
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jspm', 'mocha', 'sinon-chai'],
