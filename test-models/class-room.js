cfd96ff6ae1cad6cac63c66bad1326c910af29e6
Bill Rawlinson <bill.rawlinson@sbcs.com>
2017-01-04 16:22:01 -0500

    added for testing

---
+++ test-models/class-room.js
@@ -0,0 +1,10 @@
+import BaseModel from './base-model';
+
+export default class ClassRoom extends BaseModel {
+
+  constructor(data, referentialDataService) {
+    super('ClassRoom', data, referentialDataService);
+
+  }
+
+}
