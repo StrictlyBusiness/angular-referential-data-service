c421e44fcf5916e2635614aabd3eec8b122779f9
Bill Rawlinson <bill.rawlinson@sbcs.com>
2018-04-30 15:27:48 -0400

    revert karma so tests tun v1.0.10

--- config.js
+++ config.js
@@ -13,21 +13,21 @@ System.config({
   },
 
   map: {
-    'angular': 'github:angular/bower-angular@1.4.4',
-    'angular-cache-service': 'github:StrictlyBusiness/angular-cache-service@1.0.0',
-    'angular-sanitize': 'github:angular/bower-angular-sanitize@1.4.4',
+    'angular': 'github:angular/bower-angular@1.6.10',
+    'angular-cache-service': 'github:StrictlyBusiness/angular-cache-service@1.0.2',
+    'angular-sanitize': 'github:angular/bower-angular-sanitize@1.6.10',
     'babel': 'npm:babel-core@5.8.22',
     'babel-runtime': 'npm:babel-runtime@5.8.20',
-    'bluebird': 'npm:bluebird@2.9.34',
+    'bluebird': 'npm:bluebird@3.5.1',
     'core-js': 'npm:core-js@1.1.0',
     'lodash': 'npm:lodash@3.10.1',
-    'github:StrictlyBusiness/angular-cache-service@1.0.0': {
-      'babel': 'npm:babel-core@5.8.22',
-      'bluebird': 'npm:bluebird@2.9.34',
-      'lodash': 'npm:lodash@3.10.1'
+    'github:StrictlyBusiness/angular-cache-service@1.0.2': {
+      'babel': 'npm:babel-core@5.8.38',
+      'bluebird': 'npm:bluebird@3.5.1',
+      'lodash': 'npm:lodash@4.17.10'
     },
-    'github:angular/bower-angular-sanitize@1.4.4': {
-      'angular': 'github:angular/bower-angular@1.4.4'
+    'github:angular/bower-angular-sanitize@1.6.10': {
+      'angular': 'github:angular/bower-angular@1.6.10'
     },
     'github:jspm/nodelibs-process@0.1.1': {
       'process': 'npm:process@0.10.1'
@@ -35,7 +35,7 @@ System.config({
     'npm:babel-runtime@5.8.20': {
       'process': 'github:jspm/nodelibs-process@0.1.1'
     },
-    'npm:bluebird@2.9.34': {
+    'npm:bluebird@3.5.1': {
       'process': 'github:jspm/nodelibs-process@0.1.1'
     },
     'npm:core-js@1.1.0': {
